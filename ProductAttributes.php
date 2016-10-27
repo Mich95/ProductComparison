04766a026c27f66d0690aec11cca898f4e5d841b
mish <michellerunham@live.com>
2016-10-28 00:55:35 +1100

    Files updated

--- ProductAttributes.php
+++ ProductAttributes.php
@@ -50,7 +50,62 @@
 		die ("Invalid query");
 	}
 	
-	$headers = array("Attribute Name", "Value1", "Value2");
+	
+	
+	#function to get name of a product
+	function get_name($prod_id){
+		global $dbo;
+		
+		# Construct an SQL query as multiple lines for readability
+		$query1 = "SELECT PROD_NAME";
+		# Append to the $query string - note the required space
+		$query1 .= " FROM PRODUCT";
+		# ID of product
+		$query1 .= " WHERE PROD_ID = '".$prod_id."'";
+
+		# Run the query, returning a PDOStatement object - see http://www.php.net/manual/en/class.pdostatement.php
+		# Notice, this statement will throw a PDOException object if any problems - see http://www.php.net/manual/en/class.pdoexception.php
+		try {
+			$statement1 = $dbo->query($query1);
+		}
+		# Provide the exception handler - in this case, just print an error message and die,
+		# but see the provided default exception handler in common_db.php, which logs to the Apache error log
+		catch (PDOException $ex) {
+			echo $ex->getMessage();
+			die ("Invalid query");
+		}
+		
+		$row1 = $statement1->fetch();
+		return $row1[0];
+	}
+	
+	
+	#function to get price for a product
+	function get_price($prod_id){
+		global $dbo;
+		
+		# Construct an SQL query as multiple lines for readability
+		$query1 = "SELECT PRPR_PRICE";
+		# Append to the $query string - note the required space
+		$query1 .= " FROM PRODPRICES";
+		# ID of product
+		$query1 .= " WHERE PRPR_PROD_ID = '".$prod_id."'";
+
+		# Run the query, returning a PDOStatement object - see http://www.php.net/manual/en/class.pdostatement.php
+		# Notice, this statement will throw a PDOException object if any problems - see http://www.php.net/manual/en/class.pdoexception.php
+		try {
+			$statement1 = $dbo->query($query1);
+		}
+		# Provide the exception handler - in this case, just print an error message and die,
+		# but see the provided default exception handler in common_db.php, which logs to the Apache error log
+		catch (PDOException $ex) {
+			echo $ex->getMessage();
+			die ("Invalid query");
+		}
+		
+		$row1 = $statement1->fetch();
+		return $row1[0];
+	}
 	
 	#function to get attribute values for a product
 	function get_value($attname,$prod_id){
@@ -84,18 +139,36 @@
 		$row1 = $statement1->fetch();
 		return $row1[0];
 	}
+	
+	$headers = array(get_name(5),get_name(7));
+	$prices = array(get_price(5),get_price(7));
 ?>
 <!-- Mixed-up HTML and embedded bits of PHP from here on; read the tags carefully -->
 <!-- Print the table headers, with 2px borders around cells so you can see the structure -->
 <strong><H3>Product Comparison</H3></strong>
 <br>
 <TABLE BORDER=2>
-<TR>
-<!-- First, print the column headers -->
-<?php for($j=0; $j < sizeof($headers); $j++) { ?> <!-- see http://www.php.net/manual/en/pdostatement.columncount.php -->
-	<TH><?php echo $headers[$j] ?></TH>
-<?php	} ?>
-   </TR>
+	<!-- First row -->
+	<TR>
+		<!-- Print 'attributes' in first column-->
+		<TH>Attributes</TH>
+		<!-- Print names of products being compared -->
+		<?php for($j=0; $j < sizeof($headers); $j++) { ?> <!-- see http://www.php.net/manual/en/pdostatement.columncount.php -->
+			<TH><?php echo $headers[$j] ?></TH>
+		<?php	} ?>
+	</TR>
+	
+	<!-- Second row -->
+	<TR>
+		<!-- Print 'price' under attribute-->
+		<TH>Price</TH>
+		<!-- Print prices -->
+		<?php for($j=0; $j < sizeof($prices); $j++) { ?> 
+			<TH>$<?php echo $prices[$j]?></TH>
+		<?php	} ?>
+	</TR>
+   
+   
 <?php
 	# Print the rest of the table
 	# fetch() returns an array (by default, both indexed and name-associated) of result values for the row
@@ -110,8 +183,26 @@
 			
 			<!-- Fill cells with Attribute name, product1 value, product2 value -->
 		    <TD><?php echo $row[1]?></TD>			
-			<TD><?php echo $val1?></TD>
-			<TD><?php echo $val2?></TD>
+			<TD>
+				<?php 
+				# check product has attribute, otherwise indicate it does not
+				if($val1!=""){
+					echo $val1;
+				} else {
+					echo "----";
+				};
+				?>
+			</TD>
+			<TD>
+				<?php 
+				# check product has attribute, otherwise indicate it does not
+				if($val2!=""){
+					echo $val2;
+				} else {
+					echo "----";
+				};
+				?>
+			</TD>
 			
 		</TR>
 <?php	}
---
+++ ProductsTest.php
@@ -0,0 +1,11 @@
+<HTML>
+<HEAD>
+<TITLE> Products checkboxes</TITLE>
+
+<FORM METHOD="post" ACTION="ProductAttributes.php">
+    Computer A
+    <input type="checkbox" name="computerA" value=1 />
+    <input type="submit" name="formSubmit" value="Submit" />
+</FORM>
+</BODY>
+</HTML>
