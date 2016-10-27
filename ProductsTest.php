d043d1650fdc19a6696d3957998a75eb693fd610
Michelle95 <michellerunham@live.com>
2016-10-28 09:52:21 +1100

    Updated

--- ProductsTest.php
+++ ProductsTest.php
@@ -1,11 +1,16 @@
 <HTML>
 <HEAD>
 <TITLE> Products checkboxes</TITLE>
-
+<strong><H3>Select 2 or more products to compare:</H3></strong>
+<br>
 <FORM METHOD="post" ACTION="ProductAttributes.php">
-    Computer A
-    <input type="checkbox" name="computerA" value=1 />
-    <input type="submit" name="formSubmit" value="Submit" />
+    Dell XPS 13 <input type="checkbox" name="DELLXPS13" value=2 /></br>
+	Alienware 17 <input type="checkbox" name="ALIENWARE17" value=4 /></br>
+	HP Spectre <input type="checkbox" name="HPSPECTRE13" value=5 /></br>
+	HP Omen 17 <input type="checkbox" name="HPOMEN17" value=6 /></br>
+	Lenovo IdeaPad 110 <input type="checkbox" name="LENOVOIP110" value=7 /></br>
+	Apple MacBook Air 13 <input type="checkbox" name="MBAIR13" value=8 /></br></br>
+    <input type="submit" />
 </FORM>
 </BODY>
 </HTML>
